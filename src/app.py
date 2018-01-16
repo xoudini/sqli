c757664180a6cbc82cff445c960270a5f8526280
Dan Lindholm <main@xoudini.com>
2018-01-16 11:00:13 +0200

    Add entry point to app.py

--- src/app.py
+++ src/app.py
@@ -5,3 +5,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+
+### Entry point.
+
+if __name__ == "__main__":
+    # Switch methods for deployment.
+	# app.run(host='0.0.0.0')
+    app.run()
