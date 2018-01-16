16a36af3402fe17dddc143a884cb905eae550f2c
Dan Lindholm <main@xoudini.com>
2018-01-16 11:30:39 +0200

    Basic database wrapper.

---
+++ src/utilities/database.py
@@ -0,0 +1,34 @@
+import psycopg2 as pg
+
+class Database:
+
+    def __init__(self, database: str, user: str = None, password: str = None, host: str = None, port: int = None):
+        paramlist = []
+
+        if database is not None:
+            paramlist.append("dbname=" + database)
+        
+        if user is not None:
+            paramlist.append("user=" + user)
+        
+        if password is not None:
+            paramlist.append("password=" + password)
+        
+        if host is not None:
+            paramlist.append("host=" + host)
+        
+        if port is not None:
+            paramlist.append("port=" + str(port))
+        
+        self.dsn = " ".join(paramlist)
+
+    def execute_query(self, sql: str):
+        try:
+            with pg.connect(dsn=self.dsn) as connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(sql)
+                    return cursor.fetchall()
+
+        except Exception as e:
+            print(e)
+    
