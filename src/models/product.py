91c466cfbc1222cbe15922242cac9c700f179c75
Dan Lindholm <main@xoudini.com>
2018-01-18 16:07:52 +0200

    Add product model.

---
+++ src/models/product.py
@@ -0,0 +1,11 @@
+
+
+class Product:
+
+    def __init__(self, uid, name, description, price, stock_left):
+        self.uid = uid
+        self.name = name
+        self.description = description
+        self.price = price
+        self.stock_left = stock_left
+        
