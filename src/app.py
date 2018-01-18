37f0d5bb63b09c8d94bf4b7223adf8beaa9d5677
Dan Lindholm <main@xoudini.com>
2018-01-18 16:17:53 +0200

    Injectable search query implementation.

--- src/app.py
+++ src/app.py
@@ -1,11 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+
+from src.utilities.database import Database
+from src.models.product import Product
 
 app = Flask(__name__)
+db = Database('legit')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return redirect(url_for('products'))
 
+@app.route('/products')
+def products():
+    keyword = request.args.get('search', '')
+    keyword = keyword.strip().lower()
+
+    print("Keyword:", keyword)
+
+    rows = db.execute_query(
+        "SELECT * FROM Product WHERE lower(name) LIKE ('%" + keyword + "%') ORDER BY name;"
+    )
+
+    products = []
+
+    try:
+        for row in rows:
+            product = Product(row[0], row[1], row[2], row[3], row[4])
+            products.append(product)
+    except Exception as e:
+        return "ERROR: " + str(e)
+
+    count = len(products) if keyword else None
+
+    return render_template('index.html', products=products, count=count, keyword=keyword)
+
+@app.route('/products/<uid>')
+def product(uid):
+    rows = db.execute_query(
+        "SELECT * FROM Product WHERE id = " + str(uid) + ";"
+    )
+
+    try:
+        row = rows.pop(0)
+        product = Product(row[0], row[1], row[2], row[3], row[4])
+        return render_template('product.html', product=product)
+
+    except Exception as e:
+        return "ERROR: " + str(e)
 
 
 ### Entry point.
